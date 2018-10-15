8aab141b687896d1a5662a87bd1f78698dac91b8
Matti van de Weem <mvdweem@gmail.com>
2018-10-15 15:15:11 +0200

    build

---
+++ build/precache-manifest.5939f8d8dd98c6e8ae623f7755c2b945.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "144c3f7b2ae3d064c304",
+    "url": "/static/js/main.144c3f7b.chunk.js"
+  },
+  {
+    "revision": "8a932c6994f11f8529f4",
+    "url": "/static/js/1.8a932c69.chunk.js"
+  },
+  {
+    "revision": "144c3f7b2ae3d064c304",
+    "url": "/static/css/main.f78ad11f.chunk.css"
+  },
+  {
+    "revision": "bbb53b25f47ce62779a8506dcaf069e8",
+    "url": "/index.html"
+  }
+];
