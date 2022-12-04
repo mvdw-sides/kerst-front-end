904a099f61f549724c4dd63bcf2eec380fab5f55
Matti van de Weem <mvdweem@gmail.com>
2022-12-04 13:56:39 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { Route, BrowserRouter } from "react-router-dom";
 ReactDOM.render((
     <div>
         <BrowserRouter>
-            <Route path="/" render={ props => <App {...props} base="https://vdweem-kerst-api.herokuapp.com"/>} public />
+            <Route path="/" render={ props => <App {...props} base="https://api.kerst.vdweem.xyz"/>} public />
         </BrowserRouter>
     </div>
 ), document.getElementById('root'));
